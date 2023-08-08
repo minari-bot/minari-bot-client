e0300a44ec6c4a6517ab3d5430b9b6c33df49d12
poodly <qwe0238@naver.com>
2023-08-08 18:09:00 +0900

    구글 로그인 임시 버튼

--- client/src/components/auth/SignIn.tsx
+++ client/src/components/auth/SignIn.tsx
@@ -96,7 +96,7 @@ function SignIn({signInError, setSignInError} : SignInProps){
                 {/* <span>비밀번호 찾기</span> */}
             </Info>
         </Form>
-            <button onClick={() => {window.location.href = 'http://localhost:5000/auth/google/'}}> 구글 로그인 </button>
+            <button onClick={() => {window.location.href = 'http://minari-api-prod.fly.dev/auth/google/'}}> 구글 로그인 </button>
     </Wrapper>
 
 }
