e235e62db2c48c717e93569cc8c6d22441428979
geha <ospac111@gmail.com>
2023-08-13 18:35:53 +0900

    feat: 구글로그인 수정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,16 +5,19 @@ import { ThemeProvider } from 'styled-components';
 import router from './router/Router'
 import theme from './styles/theme';
 import {RecoilRoot} from 'recoil' 
+import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID as string}>
       <ThemeProvider theme={theme}>
         <RecoilRoot>
             <RouterProvider router={router}/>
         </RecoilRoot>
       </ThemeProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
